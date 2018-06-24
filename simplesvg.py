b13b26c4d109ab76e2b876f17fbb6256d698be11
Chris Fuller <cfuller800@thinkingplanet.net>
2018-06-24 08:39:41 -0500

    Flipped the sense of Pytion 2/3 compatibility

--- simplesvg.py
+++ simplesvg.py
@@ -10,7 +10,7 @@ even the Standard Libary! EDIT: Oh drat. Almost."""
 
 import sys
 
-_PY3 = sys.version_info[0] == 3
+_PY2 = sys.version_info[0] < 3
 
 del sys
 
@@ -39,24 +39,24 @@ class Element(dict):
         return _copy(self)
 
     def __str__(self):
-        return '<%s %s%s\n' % (self._tag, ' '.join(['%s="%s"' % i for i in self.iteritems()]),
+        return '<%s %s%s\n' % (self._tag, ' '.join(['%s="%s"' % i for i in self.items()]),
             '>\n%s\n</%s>' % ('\n'.join(map(str, self._children)), self._tag) if self._children else ' />')
 
-    if _PY3:
+    if _PY2:
         @property
-        def iteritems(self):
-            return self.items
+        def items(self):
+            return self.iteritems
 
 class Style(dict):
     """What it says on the tin. A dictionary representation of the style attribute.
     Not to be confused with the Style element!"""
     def __str__(self):
-        return ';'.join(['%s:%s' % i for i in self.iteritems()])
+        return ';'.join(['%s:%s' % i for i in self.items()])
 
-    if _PY3:
+    if _PY2:
         @property
-        def iteritems(self):
-            return self.items
+        def items(self):
+            return self.iteritems
 
 class StyledElement(Element):
     def __init__(self, tag, **attrs):
