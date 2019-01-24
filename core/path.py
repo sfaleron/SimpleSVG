4fdff1c9b21c32592b570955c383d37eb362102c
Chris Fuller <cfuller800@thinkingplanet.net>
2019-01-24 11:37:42 -0600

    Removed an incorrect adaptation made when support for absolute paths was implemented. Added support for horizontal/vertical line paths

--- core/path.py
+++ core/path.py
@@ -9,22 +9,28 @@ class PathOps(object):
         self._host     = host
         self._absolute = absolute
 
+    def _fmt(self, opIn, fmtIn=None):
+        opOut = opIn.upper() if self._absolute else opIn
+
+        return opOut if fmtIn is None else '{} {}'.format(opOut, fmtIn)
+
     def _op(self, cmd, formatOnly):
         if formatOnly:
             return cmd
         else:
             self._host.append(cmd)
 
-    def _fmt(self, opIn, fmtIn=None):
-        opOut = opIn.upper() if self._absolute else opIn
-
-        return opOut if fmtIn is None else '{} {}'.format(opOut, fmtIn)
-
     def moveTo(self, pt, formatOnly=False):
         return self._op(self._fmt('m', '{} {}').format(*pt), formatOnly)
 
     def lineTo(self, pt, formatOnly=False):
         return self._op(self._fmt('l', '{} {}').format(*pt), formatOnly)
+
+    def lineToH(self, x, formatOnly=False):
+        return self._op(self._fmt('h', '{}').format(x), formatOnly)
+
+    def lineToV(self, y, formatOnly=False):
+        return self._op(self._fmt('v', '{}').format(y), formatOnly)
 
     def arcTo(self, pt, rx, ry, rot, bigArc=False, incAngle=True, formatOnly=False):
         """https://www.w3.org/TR/SVG/implnote.html#ArcImplementationNotes"""
@@ -40,24 +46,18 @@ class PathOps(object):
 
 
 class Path(StyledElement):
-    def __init__(self, initial_pos=None, **attrs):
+    def __init__(self, initial_pos, **attrs):
         StyledElement.__init__(self, 'path', **attrs)
-        self._started = False
-        self._steps   = []
 
         self.rel = PathOps(self, False)
         self.abs = PathOps(self, True)
 
-        if initial_pos is not None:
-            self.append('M {},{}'.format(*initial_pos))
+        self._steps = []
+
+        self.abs.moveTo(initial_pos)
 
     def append(self, e):
-        self._started = self._started or e.startswith('M')
-
-        if self._started:
-            self._steps.append(e)
-        else:
-            raise ValueError("Path must begin with 'M' command.")
+        self._steps.append(e)
 
     def lineToMany(self, start, many, formatOnly=False):
         """Absolute-relative combination"""
